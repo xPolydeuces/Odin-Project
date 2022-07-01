d9e301fee8ad9b357214dbd47314653a678f7935
Polydeuces <bartosz.kot.00@gmail.com>
2022-07-01 15:57:29 +0200

    Add odin-javascript-fundamentals

---
+++ Lessons/odin-javascript-fundamentals/script.js
@@ -0,0 +1,14 @@
+console.log(12 + 25);               // exercise 1
+console.log(1 + 2 + 3 + 4 + 5 + 6); // exercise 2
+console.log((4 + 6 + 9) / 77);      // exercise 3
+
+let a = 10;                         // exercise 4
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+
+const MAX = 57;                     // exercise 5
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log(percentage)
