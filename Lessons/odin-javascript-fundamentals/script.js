fe9131366252065eb245e32f9140f32bb4952ac9
Polydeuces <bartosz.kot.00@gmail.com>
2022-07-03 18:30:21 +0200

    Solve functions module

--- Lessons/odin-javascript-fundamentals/script.js
+++ Lessons/odin-javascript-fundamentals/script.js
@@ -12,3 +12,31 @@ const MAX = 57;                     // exercise 5
 let actual = MAX - 13;
 let percentage = actual / MAX;
 console.log(percentage)
+
+// functions
+
+function add7(number){              // exercise 1
+    console.log(number + 7);
+}
+
+add7(5);
+
+function multiply(multiplicand, multiplier){
+    console.log(multiplicand * multiplier);
+}
+
+multiply(4, 5);
+
+function capitalize(string){
+    const lowerString = string.toLowerCase().slice(1);
+    const firstChar = string.charAt(0).toUpperCase();
+    console.log(firstChar + lowerString);
+}
+
+capitalize("hEllO tHere");
+
+function lastLetter(string){
+    console.log(string.slice(-1));
+}
+
+lastLetter("abcd");
