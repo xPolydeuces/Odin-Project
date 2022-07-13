4a376359f4cfb3aaec62dddda1242b63d622349d
Polydeuces <bartosz.kot.00@gmail.com>
2022-07-13 23:49:29 +0200

    Refactor code and add images

--- odin-rock-paper-scissors/script.js
+++ odin-rock-paper-scissors/script.js
@@ -6,19 +6,32 @@ const buttons = document.querySelectorAll('button');
 const roundResult = document.querySelector('.roundResult');
 const displayPlayerScore = document.querySelector('.displayPlayerScore');
 const displayComputerScore = document.querySelector('.displayComputerScore');
+const computerChoiceImg = document.querySelector('.computerChoiceImg');
 buttons.forEach(button => button.addEventListener('click', playerPlay))
 
 // Functions
+
+function capitalize(string){
+    const lowerString = string.toLowerCase().slice(1);
+    const firstChar = string.charAt(0).toUpperCase();
+    return firstChar + lowerString;
+}
 
 function computerPlay(){
     // function returns "Rock", "Paper" or "Scissors" value, chosen randomly
     const choice = Math.floor(Math.random() * 3);
     switch (choice) {
         case 0:
+            computerChoiceImg.firstChild.src = 'https://static.wikia.nocookie.net/jet/images/b/b3/Guu2.jpg/revision/latest?cb=20061118005419';
+            computerChoiceImg.firstChild.alt = 'rock';
             return "Rock";
         case 1:
+            computerChoiceImg.firstChild.src = 'https://static.wikia.nocookie.net/jet/images/6/6d/Paa2.jpg/revision/latest?cb=20061118005432';
+            computerChoiceImg.firstChild.alt = 'paper';
             return "Paper";
         case 2:
+            computerChoiceImg.firstChild.src = 'https://static.wikia.nocookie.net/jet/images/0/0e/Choki2.jpg/revision/latest?cb=20061118005407';
+            computerChoiceImg.firstChild.alt = 'scissors';
             return "Scissors";
     }
 }
@@ -26,7 +39,7 @@ function computerPlay(){
 function playerPlay(e){
     // function prompts player for valid value, if the value is incorrect, it prompts user again
     if (gameEnd) return;
-    let playerSelection = this.innerHTML;
+    const playerSelection = capitalize(this.className);
     game(playerSelection, computerPlay());
 }
 
@@ -65,8 +78,8 @@ function playRound(playerSelection, computerSelection){
 function game(playerSelection, computerSelection){
     // main function of the game, plays 5 rounds and returns the winner
     roundResult.innerHTML = playRound(playerSelection, computerSelection);
-    if (playerScore == 5){roundResult.innerHTML = "Player wins!"; gameEnd=true} 
+    if (playerScore == 5){roundResult.innerHTML = "You win!"; gameEnd=true} 
     if (computerScore == 5){roundResult.innerHTML = "Computer wins!"; gameEnd=true}
-    displayPlayerScore.innerHTML = `Player: ${playerScore}`;
+    displayPlayerScore.innerHTML = `You: ${playerScore}`;
     displayComputerScore.innerHTML = `Computer: ${computerScore}`;
 }
