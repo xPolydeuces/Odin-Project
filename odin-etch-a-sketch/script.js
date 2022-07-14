fda15b3ec7932ac63ebded875453c3168831d6bf
Polydeuces <bartosz.kot.00@gmail.com>
2022-07-14 14:59:36 +0200

    Add slider to change the size of the grid

--- odin-etch-a-sketch/script.js
+++ odin-etch-a-sketch/script.js
@@ -1,20 +1,32 @@
 const gridContainer = document.querySelector('#grid-container');
-const gridSize = 16;
+const slider = document.querySelector('.slider');
+const value = document.querySelector('.value');
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-for(let i = 0; gridSize*gridSize > i; i++){
-    const gridElement = document.createElement('div');
-    gridElement.classList.add('grid-element');
-    gridElement.style.height = 32/gridSize+'rem';
-    gridElement.style.width = 32/gridSize+'rem';
-    gridElement.addEventListener('mouseover', fillGridElement);
-    gridElement.addEventListener('mousedown', fillGridElement);
-    gridContainer.appendChild(gridElement);
+function gridCreate(){
+    let gridSize = slider.value;
+    value.innerHTML = gridSize;
+    gridContainer.innerHTML = '';
+    for(let i = 0; gridSize*gridSize > i; i++){
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.style.height = 32/gridSize+'rem';
+        gridElement.style.width = 32/gridSize+'rem';
+        gridElement.addEventListener('mouseover', fillGridElement);
+        gridElement.addEventListener('mousedown', fillGridElement);
+        gridContainer.appendChild(gridElement);
+    }
 }
 
 function fillGridElement (e) {
     if (e.type === 'mouseover' && !mouseDown) return;
     e.target.classList.add('filled');
 }
+
+slider.oninput = function() {
+    gridCreate();
+}
+
+gridCreate();
